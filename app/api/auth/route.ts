07bc8ba79f9ed0abbeb114305bfaf41c3e585508
jonathanlucas1489 <jonathan.morais@ccc.ufcg.edu.br>
2025-01-30 12:43:46 -0300

    debug vercel cache

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,8 @@
 import ImageKit from "imagekit";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 const imagekit = new ImageKit({
   publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
   privateKey: process.env.NEXT_PUBLIC_VATE_KEY!,
@@ -10,7 +12,9 @@ const imagekit = new ImageKit({
 export async function GET() {
   try {
     const expireTimestamp = Math.floor(Date.now() / 1000) + 3400;
+    console.log("New expireTimestamp:", expireTimestamp);
     const authParams = imagekit.getAuthenticationParameters(undefined, expireTimestamp);
+
     return NextResponse.json(authParams, {
       headers: {
         'Cache-Control': 'no-store', 
